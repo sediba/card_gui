23e0e4d3bb481ecadd49231b1cc0be5d6cbf40c5
sedi <sedi@am-a402b9422192.intern>
2020-07-23 17:11:42 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+import io from "socket.io-client";
 import './App.css';
 
-function App() {
+const socket = io.connect("http://192.168.0.247:3233");
+
+class App extends React.Component {
+  state = {
+    enter: "Place the card/QR code",
+    background: "#282c34"
+  }
+  componentDidMount(){
+    socket.on("verification", data => {
+      this.setState({ enter: data.open ? "Welcome" : data.error, background: data.open ? "#a6b401" : "#d50102" });
+      setTimeout(() => {
+        this.setState({ enter: "Place the card/QR code", background: "#282c34" })
+      }, 2000)
+    });
+  }
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <header className="App-header" style={{backgroundColor: this.state.background}}>
+        <img src="output-onlinepngtools(1).png" className="App-logo" alt="logo" />
+        <p style={{fontSize: "1.6em"}}>
+          {this.state.enter}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
+  }
 }
 
 export default App;
